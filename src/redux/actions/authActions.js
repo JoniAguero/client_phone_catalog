9b701a95577abc164091726e66199fb8e6820cda
Joni <jonathan@eelptex.com>
2020-12-21 00:08:56 +0100

    close modal after register

--- src/redux/actions/authActions.js
+++ src/redux/actions/authActions.js
@@ -30,23 +30,29 @@ export const startLogin = (email, password) => {
 
 export const startRegister = (email, password, name) => {
   return async (dispatch) => {
-    const resp = await fetchWithoutToken(
-      "auth/new",
-      "POST",
-      { email, password, name }
-    )
+    const resp = await fetchWithoutToken("auth/new", "POST", {
+      email,
+      password,
+      name,
+    })
     const body = await resp.json()
+    try {
+      if (body.ok) {
+        localStorage.setItem("token", body.token)
+        localStorage.setItem("token-init-date", new Date().getTime())
 
-    if (body.ok) {
-      localStorage.setItem("token", body.token)
-      localStorage.setItem("token-init-date", new Date().getTime())
-
-      dispatch(
-        login({
-          uid: body.uid,
-          name: body.name,
-        })
-      )
+        dispatch(
+          login({
+            uid: body.uid,
+            name: body.name,
+          })
+        )
+        dispatch(uiCloseModal())
+      } else {
+        dispatch(setError(body.msg))
+      }
+    } catch (error) {
+      dispatch(setError(error))
     }
   }
 }
