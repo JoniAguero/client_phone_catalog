77ad5673443219cc8b4dc6a5e5827513d61d3574
Joni <jonathan@eelptex.com>
2020-12-21 19:23:10 +0100

    loading home page fixed

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,45 +1,47 @@
 import React, { useEffect, useState } from "react"
 import { getPhones } from "../../redux/actions/phonesActions"
 import { useDispatch, useSelector } from "react-redux"
-import './Home.css';
-import Loading from "../../components/Loading";
-import PlaceholderCard from "../../components/Loading/PlaceholderCard";
-import CardPhone from "../../components/CardPhone";
-import { Card } from "semantic-ui-react";
-import NoDataFound from "../../components/NoDataFound";
+import "./Home.css"
+import Loading from "../../components/Loading"
+import PlaceholderCard from "../../components/Loading/PlaceholderCard"
+import CardPhone from "../../components/CardPhone"
+import { Card } from "semantic-ui-react"
+import NoDataFound from "../../components/NoDataFound"
 
 export const Home = () => {
-
   const [loading, setLoading] = useState(true)
   const dispatch = useDispatch()
-  const { phones } = useSelector( state => state )
-  
+  const { phones } = useSelector((state) => state)
+
   useEffect(() => {
     dispatch(getPhones())
     setTimeout(() => {
       setLoading(false)
-    }, 1000);
+    }, 1000)
   }, [dispatch])
 
-  if ( loading ) {
+  if (loading) {
     return (
       <div className="container-home">
         <Loading repetitions={2}>
           <PlaceholderCard />
         </Loading>
-      </div> 
-    );
+      </div>
+    )
   }
 
   return (
     <div className="container-home">
       <Card.Group itemsPerRow={3} stackable doubling>
-        { 
-          phones.list.length === 0 ? <NoDataFound /> : 
-          phones.list.map(element => 
-            <CardPhone phone={element} key={element._id}/>
-          )
-        }
+        {phones.list.length === 0 ? (
+          <Loading repetitions={2}>
+            <PlaceholderCard />
+          </Loading>
+        ) : (
+          phones.list.map((element) => (
+            <CardPhone phone={element} key={element._id} />
+          ))
+        )}
       </Card.Group>
     </div>
   )
